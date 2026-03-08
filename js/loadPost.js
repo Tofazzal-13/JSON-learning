caee37fb86f5d16ba624b638ecf67cc852186159
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-08 15:22:17 +0600

    json data display on ui

--- js/loadPost.js
+++ js/loadPost.js
@@ -1,24 +1,33 @@
 const loadPost = () => {
     const url = "https://jsonplaceholder.typicode.com/posts"
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayPost(data)
-    )
+        .then(res => res.json())
+        .then(data => displayPost(data)
+        )
 }
 
+/* {
+"userId": 1,
+"id": 1,
+"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+"body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+} */
 
 const displayPost = (posts) => {
-     const postContainer = document.getElementById('post_contianer');
-     
-     
-
+    const postContainer = document.getElementById("postContainer")
+    postContainer.innerHTML = "  "
     posts.forEach(post => {
-        // console.log(post.body);  
-        const li = document.createElement("li");
-        li.innerText = post.title;
+        const postCard = document.createElement("div");
+        postCard.innerHTML = `
+        <div class="post_card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
         
-        postContainer.appendChild(li)
-        
+        `
 
-    }); 
+        postContainer.append(postCard)
+    });
 }
+
+loadPost()
