8c1de96c2c830a84555a6f5aa7b4ff078a6d2f86
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-08 14:28:21 +0600

    json fetch data

--- js/json.js
+++ js/json.js
@@ -3,3 +3,18 @@ const loadData = () => {
       .then(response => response.json())
       .then(json => console.log(json))
 }
+
+
+const loadPost = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => displayPost(data)
+    )
+}
+
+
+const displayPost = (posts) => {
+    posts.forEach(post => {
+        console.log(post);  
+    }); 
+}
